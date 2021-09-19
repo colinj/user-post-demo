92d1d8aef463bb83ca15652b31de4525cb296c04
Colin Johnsun <colin.johnsun@gmail.com>
2021-09-19 18:20:22 +1000

    feat: Display user posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <script lang="ts">
 import { defineComponent, computed } from "vue";
 import { useStore } from "vuex";
-import { User } from "@/typings";
+import { User, Post } from "@/typings";
 
 export default defineComponent({
   setup() {
     const userId = 3;
     const store = useStore();
     const user = computed(() => store.state.user as User);
+    const posts = computed(() => store.state.posts as Array<Post>);
 
     store.dispatch("getUser", userId);
+    store.dispatch("getPosts", userId);
 
     return {
       user,
+      posts,
     };
   },
 });
@@ -21,6 +24,10 @@ export default defineComponent({
 <template>
   <div v-if="user.name">
     <h2>Hello {{ user.name }}</h2>
+    <hr />
+    <div v-for="post in posts" :key="post.id">
+      {{ post.title }}
+    </div>
   </div>
   <div v-else>Loading...</div>
 </template>
